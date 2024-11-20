c8f4a5aef196057bb513856ad84ca69331057476
Dale Hendricks <dalerhendricks@outlook.com>
2024-11-19 16:50:40 -0800

    Book interface

---
+++ src/lab5/Book.java
@@ -0,0 +1,11 @@
+package lab5;
+
+public interface Book {
+  String getTitle();
+
+  String toString();
+
+  void setIsAvailable(boolean isAvailable);
+
+  boolean getIsAvailable();
+}
---
+++ src/lab5/tests/PaperBookTest.java
@@ -0,0 +1,4 @@
+package lab5.tests;
+
+public class PaperBookTest {
+}
